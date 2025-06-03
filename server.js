d465de30dd239f5040c7cdc40bb71ab9d1a3df4a
JaiPrakashPS <jaiprakash.ps2023it@sece.ac.in>
2025-06-03 09:37:07 +0530

    Configured for deployment

--- server.js
+++ server.js
@@ -4,14 +4,13 @@ import cors from "cors";
 import authRoutes from "./routes/auth.js";
 import recipesRoutes from "./routes/recipes.js";
 import { connectDB } from "./config/db.js";
-import path from "path";
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
@@ -23,14 +22,7 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: "Server error" });
 });
 
-// const __dirname = path.resolve();
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "/frontend/dist")));
-//   app.get("/{*splat}", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-//   });
-// }
 
 const startServer = async () => {
   try {
